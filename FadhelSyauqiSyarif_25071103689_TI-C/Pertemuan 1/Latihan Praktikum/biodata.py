c83acc898afbbb8133639a03321e4e923169940d
SyauqiSyarif <syauqi.syarif@gmail.com>
2026-02-03 20:24:56 +0700

    biodata.py

---
+++ FadhelSyauqiSyarif_25071103689_TI-C/Pertemuan 1/Latihan Praktikum/biodata.py
@@ -0,0 +1,10 @@
+nama = 'Fadhel Syauqi Syarif'
+kelas = 'TI-C'
+NIM = 25071103689
+Universitas = 'UNIVERSITAS RIAU'
+Fakultas = 'Teknik'
+
+print ('Halo Semuanya, nama saya ' + nama, 'saya dari kelas', kelas)
+print ('dengan NIM ', str(NIM))
+print ('Saya berkuliah di ' + Universitas)
+print ('di Fakultas ' + Fakultas)
